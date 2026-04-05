eac5657ae2994bf865686f4403cfc9216942fc49
ZaytsevDmitry <zaytsev.dmitry9228@gmail.com>
2026-04-05 23:48:04 +0300

    Delete feature_toggle/backend directory

--- feature_toggle/backend/src/main/java/com/homni/featuretoggle/infrastructure/security/sso/SsoConfigurationException.java
+++
@@ -1,8 +0,0 @@
-package com.homni.featuretoggle.infrastructure.security.sso;
-
-public class SsoConfigurationException extends RuntimeException {
-
-    public SsoConfigurationException(String message) {
-        super(message);
-    }
-}
